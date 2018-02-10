bcf91b1120c0b2f05625840121398ed826370308
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-02-10 21:34:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+display_board = "Tic Tac Toe"
+
+def display_board(board)
+  puts "Row "
+
+
+board = [" "," "," "," "," "," "," "," "," "]
